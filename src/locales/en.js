8cc29c9586214c1eaf203203f2484d02b5ec6d07
vasekguryev <guryevi30041994@gmail.com>
2018-10-14 21:36:13 +0200

    Сделана страничка софта.

--- src/locales/en.js
+++ src/locales/en.js
@@ -20,15 +20,30 @@ export default {
     home: {
       mobileLaserScan: 'Mobile Laser Scan',
       mobileLaserScanThis: 'this technology of high-speed formation of three-dimensional images of or the surface of the earth in the form of point clouds, to create high-precision three-dimensional digital modelsThe use of scan data can be applied in a large number of industries, from geo-dosage architecture to computer games and virtual reality.',
-      scanVizThis: 'ScanViz is a universal solution for creating mobile laser scanning systems for vary type of works.',
+      scanVizThis: 'ScanViz is a universal solution for creating mobile laser scanning systems for vary type of works',
       scanVizThisContent: 'За несколько минут наши системы могут быть установленны на беспилотнике, на самолете, на машине, на железнодорожном транспорте, на судне или в рюкзаке. Разработанная платформа позволяет собрать не дорогую систему начального уровня с недорогими LIDAR (например Velodyne VLP16), GNSS модулями (например Piksi Multi), IMU, 360 CAMERAS чтоб вы могли начать работать уже сегодня, а в дальнейшем без труда менять отдельные устройства на приборы более высокого класса вплоть до топовых и дорогих высокоточных решений (Novatel, Z+F Profiler, Applanix). Разработанное програмное обеспечение [по управлению и сбору данных] имеет [общий для всех приборов] web интерфейс и доступно с любых устройств. Результат работы нашей систем - готовое облако точек сразу в LAS файле! Сырые данные так же доступны для вас и вашего ПО постобработки. Мы позволяем совершенствовать систему под возрастающие требования и разное оборудование без необходимости менять привычный режим работы, не нужно переходить на разные программные решения от различных производителей.',
       car: 'Car',
       drone: 'Drone',
       withoutPilot: 'UAV',
       boat: 'Boat',
       backpack: 'Backpack',
-      carContent: 'Устройство легко устанавливается на крышу автомобиля',
-      areasOfUse: 'Areas of use'
+      carContent: 'The device is easily installed on the roof of the car',
+      droneContent: 'The device is easily installed on the roof of the car',
+      withoutPilotContent: 'The device is easily installed on the roof of the car',
+      boatContent: 'The device is easily installed on the roof of the car',
+      backpackContent: 'The device is easily installed on the roof of the car',
+      areasOfUse: 'Areas of use',
+      areasOfUse_1: 'Surveying & Mapping',
+      areasOfUse_2: 'Road',
+      areasOfUse_3: 'Construction & architecture',
+      areasOfUse_4: 'Natural disasters',
+      areasOfUse_5: 'Agricultural sector',
+      areasOfUse_6: 'Maps for self-driving cars',
+      areasOfUse_7: 'Investigation of dangerous objects',
+      areasOfUse_8: 'Infrastructure control',
+      areasOfUse_9: 'Scientific research',
+      areasOfUse_10: 'Ecology & Natural resources',
+      areasOfUse_11: 'Creating digital space for computer games'
     },
     products: {
       products: 'Products',
@@ -39,6 +54,10 @@ export default {
       all: 'All',
       svMobile: 'это технология высокоскоростного формирования трехмерных изображений объектов или поверхности земли в виде облаков точек, для создания высокоточных трехмерных цифровых моделей. Использование данных сканирования может применяться в большом количестве отраслей, от геодезии и архитектуры до компьютерных игр и виртуальной реальности.',
       svUni: 'SV Uni'
+    },
+    soft: {
+      software: 'Software',
+      ourTask: 'Our task is to make laser scanning technology understandable and accessible for widespread use'
     }
   }
 }
--- src/views/Soft.vue
+++ src/views/Soft.vue
@@ -1,5 +1,81 @@
 <template>
-  <div class="soft">
-    <h1>This is an soft page</h1>
+  <div class="home">
+    <carousel></carousel>
+    <div class="container">
+      <div class="col soft-content">
+        <div class="text-center">
+          <h1>{{ $t("content.soft.software").toUpperCase() }}</h1>
+        </div>
+        <div class="row software-horizontal">
+          <div class="col-md-6 col-sm-12">
+            <img src="../assets/software_1.png" alt="">
+          </div>
+          <div class="col-md-6 col-sm-12">
+            <img src="../assets/software_2.png" alt="">
+          </div>
+          <div class="col-md-6 col-sm-12">
+            <img src="../assets/software_3.png" alt="">
+          </div>
+          <div class="col-md-6 col-sm-12">
+            <img src="../assets/software_4.png" alt="">
+          </div>
+        </div>
+        <div class="row software-vertical">
+          <div class="col-sm-4 col-md col-12">
+            <img src="../assets/software_5.png" alt="">
+          </div>
+          <div class="col-sm-4 col-md col-12">
+            <img src="../assets/software_6.png" alt="">
+          </div>
+          <div class="col-sm-4 col-md col-12">
+            <img src="../assets/software_7.png" alt="">
+          </div>
+          <div class="col-sm-4 col-md col-12">
+            <img src="../assets/software_8.png" alt="">
+          </div>
+          <div class="col-sm-4 col-md col-12">
+            <img src="../assets/software_9.png" alt="">
+          </div>
+        </div>
+        <div class="text-center">
+          <h1>{{ $t("content.soft.ourTask").toUpperCase() }}</h1>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import Carousel from '@/components/Carousel'
+
+export default {
+  name: 'products',
+  components: {
+    'carousel': Carousel
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .soft-content {
+    & > div {
+      padding: 20px 0;
+    }
+    h1, h2, h3, h4, h5, h6 {
+      background: linear-gradient(to bottom right, #24283B, #2D3B57, #2F4B6A, #2E5E79, #2F6580);
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+    }
+    img {
+      max-width:100%;
+      max-height:100%;
+    }
+    .software-horizontal,
+    .software-vertical {
+      div {
+        padding-top: 20px;
+        padding-bottom: 20px;
+      }
+    }
+  }
+</style>
